8d2c7ffbd010a0a40a6e3f103797fb53051db877
cw-ataka <ataka@chatwork.co.jp>
2020-06-29 16:31:40 +0900

    Prepare Package.swift for ls2xsR

--- Package.swift
+++ Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "ls2xsR",
+    products: [
+        .executable(name: "ls2xsR", targets: ["ls2xsR"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ls2xsR",
-            dependencies: []),
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
             name: "ls2xsRTests",
             dependencies: ["ls2xsR"]),
     ]
 )
+
+// Local Variables:
+// swift-mode:parenthesized-expression-offset: 4
+// End:
